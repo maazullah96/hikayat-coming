d10ed585d861f69412132dc37dc032d52f38a31b
Maaz Ullah <maazullahminhas96@gmail.com>
2023-12-31 13:57:58 +0500

    hikayat app analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,3 @@
-// import React from "react"
-// import { Container, Row, Col } from "react-bootstrap"
-// const App = () => {
-//   return (
-//     <Container fluid className="hikayat-background">
-//       <Row
-//         className="justify-content-center align-items-center"
-//         style={{ height: "100vh" }}
-//       >
-//         <Col md={8} className="text-center">
-//           <h1 className="display-3 text-muted">Hikayat.app is coming</h1>
-//           <p className="lead text-muted">
-//             A unique escape into the world of wisdom through stories, parables,
-//             and riddles.
-//           </p>
-//           {/* Add more content or components here */}
-//         </Col>
-//       </Row>
-//     </Container>
-//   )
-// }
-
-// export default App
-
-// import React from "react"
-// import { Container, Row, Col } from "react-bootstrap"
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-// import {
-//   faTwitter,
-//   faFacebook,
-//   faInstagram,
-//   faPinterest,
-//   faYoutube,
-// } from "@fortawesome/free-brands-svg-icons"
-
-// const App = () => {
-//   return (
-//     <Container fluid className="hikayat-background">
-//       <Row
-//         className="justify-content-center align-items-center"
-//         style={{ height: "100vh" }}
-//       >
-//         <Col md={8} className="text-center">
-//           <h1 className="display-3 text-muted">Hikayat.app is coming</h1>
-//           <p className="lead text-muted">
-//             A unique escape into the world of wisdom through stories, parables,
-//             and riddles.
-//           </p>
-//           {/* Social Media Icons */}
-//           <Row className="justify-content-center mt-4">
-//             <a href="https://twitter.com/Hikayat_App" className="social-icon">
-//               <FontAwesomeIcon icon={faTwitter} size="2x" />
-//             </a>
-//             <a
-//               href="https://www.facebook.com/profile.php?id=100088725561982"
-//               className="social-icon"
-//             >
-//               <FontAwesomeIcon icon={faFacebook} size="2x" />
-//             </a>
-//             <a
-//               href="https://www.instagram.com/hikayat_app/"
-//               className="social-icon"
-//             >
-//               <FontAwesomeIcon icon={faInstagram} size="2x" />
-//             </a>
-//             <a
-//               href="https://www.pinterest.com/hikayatapp/"
-//               className="social-icon"
-//             >
-//               <FontAwesomeIcon icon={faPinterest} size="2x" />
-//             </a>
-//             <a
-//               href="https://www.youtube.com/@Inspirational_Hikayat"
-//               className="social-icon"
-//             >
-//               <FontAwesomeIcon icon={faYoutube} size="2x" />
-//             </a>
-//           </Row>
-//         </Col>
-//       </Row>
-//     </Container>
-//   )
-// }
-
-// export default App
-
 import React from "react"
 import { Container, Row, Col } from "react-bootstrap"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
@@ -97,8 +11,13 @@ import {
 } from "@fortawesome/free-brands-svg-icons"
 
 const App = () => {
+  
   return (
-    <Container fluid className="hikayat-background">
+    <Container fluid className="hikayat-background position-relative">
+      {/* Diagonal Tie Link */}
+      <a href="https://www.example.com" className="diagonal-tie">
+        {/* You can place an image or text here */}
+      </a>
       <Row
         className="justify-content-center align-items-center"
         style={{ height: "100vh" }}
@@ -128,7 +47,7 @@ const App = () => {
             <Col xs="auto">
               <a
                 href="https://www.instagram.com/hikayat_app/"
-                className="social-icon"
+                className="social-icon instagram-icon"
               >
                 <FontAwesomeIcon icon={faInstagram} size="2xl" />
               </a>
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
+import "./index.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 
+import ReactGA from "react-ga4"
+
+const TRACKING_ID = "G-M7RXD9DF0M" // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID)
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+})
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
